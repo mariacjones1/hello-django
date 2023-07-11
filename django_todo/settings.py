6074f64886336e7ca1674da1dab197dd50df8d48
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-07-11 15:34:54 +0000

    Create app folder

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ea2hh2_qxeno@nwi)qc^u#psuetc^$pddu@6ll+%v)5tuk0jmt'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
