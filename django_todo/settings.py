47fba1f7106f8a2f2b2fc12dbbc2e7a90c00b239
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-07-18 17:39:33 +0000

    Add allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mariacjones-hellodjango-uo61don3re0.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ['mcj-hello-django-4245165a4420.herokuapp.com']
 
 
 # Application definition
